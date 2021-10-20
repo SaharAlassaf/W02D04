af0db1f42da39abec5f08c97bdaf39b9cc6afc71
sahar <mac@MacBook-Pro-mac.Dlink>
2021-10-20 23:50:32 +0300

    bootstrap

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 const content = document.querySelector("body");
+
+const navBar = document.createElement("nav");
+navBar.className = "navbar navbar-default";
+content.append(navBar);
+
+const div1 = document.createElement("div");
+div1.className = "container-fluid";
+navBar.append(div1);
+
+const div2 = document.createElement("div");
+div2.className = "navbar-header";
+div1.append(div2);
+
+
 const titel = document.createElement("h1");
-titel.innerHTML = "To do list";
-content.append(titel);
+titel.style.fontSize = "30px";
+titel.className = "navbar-brand";
+div2.append(titel);
+
+const s = document.createElement("b");
+s.innerHTML = "To do list";
+titel.append(s);
+
+const main = document.createElement("div");
+main.className = "container";
+content.append(main);
+
 
 const ulCreate = document.createElement("ul");
 ulCreate.id = "#ToDoList";
-content.append(ulCreate);
+ulCreate.className = "list-group";
+main.append(ulCreate);
 
 let toDos = ["Wake up", "Eat breakfast", "Code"];
 
 const deleteListItem = (i) => {
-    toDos = toDos.filter((todo, index) => index !== i)
+    toDos.splice(i, 1)
     renderList();
 }
 
@@ -25,28 +50,34 @@ const renderList = () => {
         const liList = document.createElement("li");
         liList.innerHTML = toDos[i];
         liList.id = "lists"
+        liList.className = "list-group-item lead";
         ulCreate.append(liList);
+
+        const div3 = document.createElement("div");
+        div3.className = "text-right";
+        liList.append(div3);
 
         const deleteBtn = document.createElement("button");
         deleteBtn.innerHTML = "Delete";
-        deleteBtn.className = "deleteBtn";
-        // deleteBtn.id = `del${i}`;
+        deleteBtn.className = "deleteBtn btn btn-danger";
         deleteBtn.addEventListener('click', () => deleteListItem(i))
-        liList.append(deleteBtn);
+        div3.append(deleteBtn);
 
         const updateBtn = document.createElement("button");
         updateBtn.innerHTML = "Update";
-        deleteBtn.className = "updateBtn";
+        updateBtn.className = "updateBtn btn btn-primary";
         updateBtn.addEventListener('click', () => updateListItem(i))
-        liList.append(updateBtn);
+        div3.append(updateBtn);
     }
 }
 renderList();
 
 const input = document.createElement("input");
 input.type = "text";
-input.id = "#input"
-content.append(input);
+input.className = "form-control form-group";
+input.id = "focusedInput"
+input.placeholder = "Add task";
+main.append(input);
 
 const addToList = (event) => {
     let text = input.value;
@@ -59,7 +90,8 @@ const addToList = (event) => {
 
 const btn = document.createElement("button");
 btn.innerHTML = "Add";
-content.append(btn);
+btn.className = "btn btn-success"
+main.append(btn);
 btn.addEventListener('click', addToList);
 
 
